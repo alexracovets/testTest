d164dcfff76692f4276bcc5ed583246a025cc902
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2023-12-15 11:07:28 +0200

    fix camera

--- src/Components/CanvasWrapper/CanvasWrapper.jsx
+++ src/Components/CanvasWrapper/CanvasWrapper.jsx
@@ -16,7 +16,7 @@ export default function CanvasWrapper({ setLoad }) {
         <Canvas
             dpr={window.devicePixelRatio}
             gl={{ preserveDrawingBuffer: true }}
-            camera={{ position: [-62.15, 76.75, 170], fov: 90, near: 0.1, far: 1000 }}
+            camera={{ position: [-62.15, 76.75, 170], fov: 60, near: 0.1, far: 1000 }}
         >
             <ambientLight intensity={2} />
             <Controls />
