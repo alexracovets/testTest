447ce39a887b38498882dcd3b54a7775f98ac7cc
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-01-29 12:27:56 +0200

    test VR

--- src/Components/CanvasWrapper/CanvasWrapper.jsx
+++ src/Components/CanvasWrapper/CanvasWrapper.jsx
@@ -1,4 +1,5 @@
 import { Canvas } from "@react-three/fiber";
+import { VRButton, XR, Controllers, Hands } from '@react-three/xr'
 
 import Annotations from './Annotations/Annotations';
 import Building from "./Models/Building/Building";
@@ -7,16 +8,25 @@ import Panorama from './Panorama/Panorama';
 
 export default function CanvasWrapper() {
     return (
-        <Canvas
-            dpr={window.devicePixelRatio}
-            gl={{ preserveDrawingBuffer: true }}
-            camera={{ fov: 90, near: 0.1, far: 1000 }}
-        >
-            <ambientLight intensity={2} />
-            <Controls />
-            <Annotations />
-            <Building />
-            <Panorama />
-        </Canvas>
+        <>
+            <VRButton />
+            <Canvas
+                dpr={window.devicePixelRatio}
+                gl={{ preserveDrawingBuffer: true }}
+                camera={{ fov: 90, near: 0.1, far: 1000 }}
+            >
+                <XR>
+                    <Controllers />
+                    <Hands />
+                    <ambientLight intensity={2} />
+                    <Controls />
+                    <Annotations />
+                    <Building />
+                    <Panorama />
+                </XR>
+
+            </Canvas>
+        </>
+
     )
 }
