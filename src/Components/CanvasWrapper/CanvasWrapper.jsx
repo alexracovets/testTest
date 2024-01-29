05151b33985ae5b1c59518dcfba8ff1cd4477fad
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-01-29 20:33:01 +0200

    fixed camera

--- src/Components/CanvasWrapper/CanvasWrapper.jsx
+++ src/Components/CanvasWrapper/CanvasWrapper.jsx
@@ -13,16 +13,17 @@ export default function CanvasWrapper() {
                 gl={{ preserveDrawingBuffer: true }}
                 camera={{ fov: 90, near: 0.1, far: 1000, position: [0, 100, 0] }}
             >
-             
+
                 <ambientLight intensity={2} />
                 {/* <Controllers /> */}
                 {/* <Controls /> */}
                 <XR>
                     <Panorama />
+                    <Annotations />
+                    <Building />
                 </XR>
-                <Annotations />
-                <Building />
-                
+
+
             </Canvas>
         </>
 
